abdc28b0628da6253d9922908737de6b25ca19d1
kulturman <arnaudbakyono@gmail.com>
2025-03-03 10:52:35 +0000

    Add other ERC20 methods

--- contracts/KulturmanToken.sol
+++ contracts/KulturmanToken.sol
@@ -1,31 +1,25 @@
 //SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
 
-// Solidity files have to start with this pragma.
-// It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.8.0;
-
+import "./IERC20.sol";
 import "hardhat/console.sol";
-
 
 // This is the main building block for smart contracts.
 // Author: kulturman
-contract KulturmanToken {
+contract KulturmanToken is IERC20 {
     // Some string type variables to identify the token.
     string public name = "My Hardhat Token";
     string public symbol = "MHT";
 
     // The fixed amount of tokens, stored in an unsigned integer type variable.
-    uint256 public totalSupply = 1000000;
+    uint256 public totalSupply = 1_000_000;
 
     // An address type variable is used to store ethereum accounts.
-    address public owner;
+    address public tokenOwner;
 
     // A mapping is a key/value map. Here we store each account's balance.
     mapping(address => uint256) public balances;
-
-    // The Transfer event helps off-chain applications understand
-    // what happens within your contract.
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    mapping(address => mapping(address => uint256)) public allowances;
 
     /**
      * Contract initialization.
@@ -34,38 +28,45 @@ contract KulturmanToken {
         // The totalSupply is assigned to the transaction sender, which is the
         // account that is deploying the contract.
         balances[msg.sender] = totalSupply;
-        owner = msg.sender;
+        tokenOwner = msg.sender;
 
         console.log('Contract deployed');
     }
 
-    /**
-     * A function to transfer tokens.
-     *
-     * The `external` modifier makes a function *only* callable from *outside*
-     * the contract.
-     */
-    function transfer(address to, uint256 amount) external {
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false`, the
-        // transaction will revert.
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+
+    function transfer(address to, uint256 amount) external returns(bool) {
         require(balances[msg.sender] >= amount, "Not enough tokens");
 
-        // Transfer the amount.
         balances[msg.sender] -= amount;
         balances[to] += amount;
 
-        // Notify off-chain applications of the transfer.
         emit Transfer(msg.sender, to, amount);
+
+        return true;
     }
 
-    /**
-     * Read only function to retrieve the token balance of a given account.
-     *
-     * The `view` modifier indicates that it doesn't modify the contract's
-     * state, which allows us to call it without executing a transaction.
-     */
-    function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(balances[from] >= amount, "Not enough tokens");
+        require(allowances[from][msg.sender] >= amount, "Allowance exceeded");
+
+        balances[from] -= amount;
+        balances[to] += amount;
+        allowances[from][msg.sender] -= amount;
+
+        emit Transfer(from, to, amount);
+        return true;
     }
 }
