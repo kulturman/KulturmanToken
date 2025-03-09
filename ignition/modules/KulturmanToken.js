228fa25284decbc582a9d4251cc21fd1f182d85f
kulturman <arnaudbakyono@gmail.com>
2025-03-09 18:24:42 +0000

    chore: forge init

--- ignition/modules/KulturmanToken.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule('KulturmanTokenModule', m => {
-    const contract = m.contract('KulturmanToken');
-
-    return { contract };
-});
